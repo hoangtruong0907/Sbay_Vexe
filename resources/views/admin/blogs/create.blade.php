cb6ba1705a381ded7bbac9f1c2046ed99e84992c
Truong <truongthanhsbay@gmail.com>
2024-08-28 13:17:05 +0700

    remove two search changes

--- resources/views/admin/blogs/create.blade.php
+++ resources/views/admin/blogs/create.blade.php
@@ -37,10 +37,7 @@
                         <textarea id="content" name="content" class="form-control"></textarea>
                     </div>
 
-                    <!-- <div class="mb-3">
-                        <label for="author" class="form-label">Người tạo</label>
-                        <input type="text" name="author" id="author" class="form-control" required>
-                    </div> -->
+                    
                     <div class="mb-3">
                         <label for="status" class="form-label">Trạng thái</label>
                         <select name="status" id="status" class="form-control" required>
