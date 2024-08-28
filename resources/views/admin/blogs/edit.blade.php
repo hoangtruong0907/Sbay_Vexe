0e91a10dd53944da58652b49d5203c19517573bf
Truong <truongthanhsbay@gmail.com>
2024-08-28 13:05:14 +0700

    user

--- resources/views/admin/blogs/edit.blade.php
+++ resources/views/admin/blogs/edit.blade.php
@@ -36,10 +36,10 @@
             <textarea id="content" name="content" class="form-control">{{ $blog->content }}</textarea>
         </div>
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="author">Người tạo</label>
             <input type="text" name="author" id="author" value="{{ $blog->author }}" class="form-control">
-        </div>
+        </div> -->
 
         <div class="form-group">
             <label for="type">Chủ đề</label>
