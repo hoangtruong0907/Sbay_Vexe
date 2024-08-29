7f74fddd3fdf427cac43f9ff3ccd8a63242929f8
Bui Tuyet Ngan <buituyetngan5904@gmail.com>
2024-08-29 09:34:10 +0700

    fix popup review

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -12,18 +12,15 @@
             <div class="blog-list" style="margin-bottom: 20px">
                 @include('admin.profile._message')
                 <div class="text container-table " style="margin-top:30px;">
-                <h4 class="heading">Danh sách bài viết</h4>
+                    <h4 class="heading">Danh sách bài viết</h4>
                     <div class="button-blog ssss">
-                    <a href="{{ route('admin.blogs.create') }}" class="btnsss">Add Blog</a>
+                        <a href="{{ route('admin.blogs.create') }}" class="btnsss">Add Blog</a>
                     </div>
                 </div>
             </div>
             <div class="box box-primary">
                 <div class="box-body">
-                <form action="{{ route('admin.blogs.index') }}" method="GET" class="search-form">
-    <input type="text" name="search" placeholder="Tìm kiếm..." value="{{  request()->input('search') }}" class="search-input">
-    <button type="submit" class="btn-search" style="border: 1px solid #22a1f9;">Search</button>
-</form>
+
                     <table width="100%" class="table table-hover" id="dataTables-example">
                         <thead>
                             <tr>
@@ -40,34 +37,36 @@
                         </thead>
                         <tbody>
                             @foreach ($allPosts as $post)
-                            <tr>
-                                <td>{{ $post->id }}</td>
-                                <td>{{ Str::limit($post->title, 15, '...') }}</td>
-                                <td class="content-cell">{{ Str::limit($post->content, 30, '...') }}</td>
-                                <td>{{ $post->created_at->format('Y-m-d H:i:s') }}</td>
-                                <td>{{ $post->updated_at->format('Y-m-d H:i:s') }}</td>
-                                <td>{{ $post->author }}</td>
-                                <td>{{ $post->type }}</td>
-                                <td>{{ ucfirst($post->status) }}</td>
-                                <td>
-                                <a href="{{ route('admin.blogs.edit', $post->id) }}" class="btn-action">
-        <i class="fas fa-pen"></i>
-    </a>
+                                <tr>
+                                    <td>{{ $post->id }}</td>
+                                    <td>{{ Str::limit($post->title, 15, '...') }}</td>
+                                    <td class="content-cell">{{ Str::limit($post->content, 30, '...') }}</td>
+                                    <td>{{ $post->created_at->format('Y-m-d H:i:s') }}</td>
+                                    <td>{{ $post->updated_at->format('Y-m-d H:i:s') }}</td>
+                                    <td>{{ $post->author }}</td>
+                                    <td>{{ $post->type }}</td>
+                                    <td>{{ ucfirst($post->status) }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.blogs.edit', $post->id) }}" class="btn-action">
+                                            <i class="fas fa-pen"></i>
+                                        </a>
 
-    <!-- Delete Button -->
-    <form action="{{ route('admin.blogs.destroy', $post->id) }}" method="POST" style="display:inline;" onsubmit="return confirm('Are you sure you want to delete this blog?');">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn-action delete">
-            <i class="fas fa-trash"></i>
-        </button>
-    </form>
+                                        <!-- Delete Button -->
+                                        <form action="{{ route('admin.blogs.destroy', $post->id) }}" method="POST"
+                                            style="display:inline;"
+                                            onsubmit="return confirm('Are you sure you want to delete this blog?');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn-action delete">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </form>
 
-    <a href="{{ route('blog.content', ['slug' => $post->slug]) }}" class="btn-action">
-        <i class="fas fa-link"></i>
-    </a>
-                                </td>
-                            </tr>
+                                        <a href="{{ route('blog.content', ['slug' => $post->slug]) }}" class="btn-action">
+                                            <i class="fas fa-link"></i>
+                                        </a>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
@@ -82,4 +81,4 @@
 
 
 
-@endsection
+    @endsection
