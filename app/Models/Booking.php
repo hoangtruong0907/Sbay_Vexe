6e995812d5e782a65e6f3ddd33de68bf47c1f6d8
Nguyen <nguyen.tran@sapotacorp.vn>
2024-10-10 10:18:44 +0700

    [10-10-24] payment function

---
+++ app/Models/Booking.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $table = 'booking';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'order_code',
+        'trip_code',
+        'seats',
+        'price',
+        'pickup_id',
+        'drop_off_info',  
+        'drop_off_point_id',   
+        'customer_name',
+        'customer_phone',
+        'customer_email',
+    ];
+}
