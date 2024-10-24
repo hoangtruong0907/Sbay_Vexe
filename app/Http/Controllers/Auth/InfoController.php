aab48ea5554bed4ce7813fe46f1588fc3b01785f
haohaoa <haohao051103@gmail.com>
2024-10-24 15:50:33 +0700

    update

--- app/Http/Controllers/Auth/InfoController.php
+++ app/Http/Controllers/Auth/InfoController.php
@@ -41,6 +41,7 @@ class InfoController extends Controller
     {
         if(auth()->check()){
             try {
+                
                 $bookings = Booking::where('user_id', auth()->user()->id)->get(); 
                 $results = []; 
                 foreach ($bookings as $booking) {
